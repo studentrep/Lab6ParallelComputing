bb52041e5f2d7e76e22a48078ca4ce353d8a9fad
tug05235 <tug05235@CIS-SE35905.tu.temple.edu>
2018-11-05 14:57:52 -0500

    tentative update

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -22,28 +22,90 @@ int main(int argc, char* argv[])
   double *bb;	/* the B matrix */
   double *cc1;	/* A x B computed using the omp-mpi code you write */
   double *cc2;	/* A x B computed using the conventional algorithm */
+ 
+  //changes
+  int aRow, aColumn, bColumn;
+  int numberSent, sender;
+  //
+
   int myid, numprocs;
   double starttime, endtime;
   MPI_Status status;
-  /* insert other global variables here */
+  
+  /* insert other global variables here */ //changes
+  double *buffer;
+  int anstype, current_row;
+  // for accumulating the sum
+  double *result;
+  //
+  
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  
   if (argc > 1) {
-    nrows = atoi(argv[1]);
-    ncols = nrows;
+    
+    //changes  
+    aRow = atoi(argv[1]);
+    aColumn = atoi(argv[2]);
+    bColumn = atoi(argv[3]);  
+      
+    //nrows = atoi(argv[1]);
+    //ncols = nrows;
+    
+    printf("The matrices' dimensions are %d x %d and %d x %d.\n", aRow, aColumn, aColumn, bColumn);
+
+     // testing if the matrices are generated correctly
+    aa = gen_matrix(a_row, a_col);
+    bb = gen_matrix(a_col, b_col);
+
+    printf("Matrix A:\n");
+    for (int i = 0; i < a_row; i++) {
+      for (int j = 0; j < a_col; j++) {
+        printf("%.1f ", aa[i * a_col + j]);
+      }
+      printf("\n");
+    }
+
+    printf("Matrix B:\n");
+    for (int i = 0; i < a_col; i++) {
+      for (int j = 0; j < b_col; j++) {
+        printf("%.1f ", bb[i * b_col + j]);
+      }
+      printf("\n");
+    }
+    
+    nrows = aRow;
+    ncols = bColumn;
+    buffer = (double *) malloc(sizeof(double) * aColumn);
+    
+    //end changes
+    
     if (myid == 0) {
       // Master Code goes here
-      aa = gen_matrix(nrows, ncols);
-      bb = gen_matrix(ncols, nrows);
-      cc1 = malloc(sizeof(double) * nrows * nrows); 
+        
+      //changes
+      //aa = gen_matrix(nrows, ncols);
+      //bb = gen_matrix(ncols, nrows);
+       
+      cc1 = malloc(sizeof(double) * nrows * ncols);
       starttime = MPI_Wtime();
+       
+       // number of rows sent
+      numberSent = 0;
+        
+       
+      //cc1 = malloc(sizeof(double) * nrows * nrows); 
+      //starttime = MPI_Wtime();
+      
+      
       /* Insert your master code here to store the product into cc1 */
       endtime = MPI_Wtime();
       printf("%f\n",(endtime - starttime));
       cc2  = malloc(sizeof(double) * nrows * nrows);
       mmult(cc2, aa, nrows, ncols, bb, ncols, nrows);
       compare_matrices(cc2, cc1, nrows, nrows);
+      
     } else {
       // Slave Code goes here
     }
